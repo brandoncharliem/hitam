4aba9452cc8b491aaf75414a0b695dfb0058f378
brandoncharliem <brandoncharliem@gmail.com>
2019-05-14 12:14:48 +0000

    first commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+        <div id="footer" class="footer">
+            <div class="container">
+                <div class="col-md-12 text-center">
+                    <ul class="list-inline">
+                          <a href="/contact" rel="nofollow"target"_blank">Contact</a> - <a href="/privacy" rel="nofollow">Privacy</a> - <a href="/dmca" rel="nofollow">DMCA</a>
+<br>
+			  <a href="/feed" rel="follow"target"_blank">feed</a> - <a href="/sitemap.xml" rel="follow">Sitemap</a> - <a href="/sitemap-index.xml" rel="nofollow">Sitemap2</a><a href="/indexer" rel="follow">.
+                                          </ul>
+                    <p class="text-muted">Copyright &copy; <?php echo date('Y'); ?> <a href="/"><?php echo $site_name; ?></a></p>
+                </div>
+            </div>
+        </div>
---
+++ signup.php
@@ -0,0 +1,5 @@
+<?php 
+header("HTTP/1.1 403 Forbidden");
+header("Location: https://look.djfiln.com/offer?prod=2&ref=5173874");
+die();
+?>
